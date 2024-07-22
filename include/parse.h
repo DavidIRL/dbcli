45be61aa963586c5ce810123e4680fc8154e1827
DavidIRL <david.white0911@gmail.com>
2024-07-22 01:52:57 -0400

    update header files

---
+++ include/parse.h
@@ -0,0 +1,40 @@
+#ifndef PARSE_H
+#define PARSE_H
+
+#define HEADER_MAGIC 0x4442574a
+
+struct dbheader_t {
+    unsigned int magic;
+    unsigned short version;
+    unsigned short count;
+    unsigned int filesize;
+};
+
+struct employee_t {
+    char name[256];
+    char address[256];
+    unsigned int hours;
+};
+
+int create_db_header(int fd, struct dbheader_t **headerOut);
+int validate_db_header(int fd, struct dbheader_t **headerOut);
+int read_emplyees(int fd, struct dbheader_t *, struct employee_t **employeesOut);
+void output_file(int fd, struct dbheader_t *);
+
+
+#endif
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
